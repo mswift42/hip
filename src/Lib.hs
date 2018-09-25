862dd764350289b6d86d91fed50dfc0d419f0582
mswift42 <martin.haesler@gmail.com>
2018-09-25 17:56:20 +0100

    Add datatype Programme.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import Text.HTML.Scalpel
+
+data Programme =  Programme { title :: String
+                            , subtitle :: String
+                            , synopsis :: String
+                            , thumbnail :: String
+                            , url :: String
+                            , index :: Int
+                            , available :: String
+                            , duration :: String } deriving (Show)
+
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
